d395de4c70b13eb56140a5f409cf1c68f5c54088
CragglesG <121510295+CragglesG@users.noreply.github.com>
2025-01-31 19:27:09 +0000

    Attempting to fix yet again with the react router vercel template

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-// vite.config.js
-import { defineConfig } from "vite";
-import { reactRouter } from "@react-router/dev/vite";
 import envCompatible from "vite-plugin-env-compatible";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild, command }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+    sourcemap: false,
+  },
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
+  },
   plugins: [
+    tailwindcss(),
     reactRouter(),
+    tsconfigPaths(),
     envCompatible(),
     sentryVitePlugin({
       org: "craigs-org",
       project: "codeboard",
     }),
   ],
-  build: {
-    sourcemap: false,
-  },
-});
+}));
